16ce1d8f4ac945f2786260877cedc1dc8ea4147b
Youngwoo Kim (Brandon) <brandon.kim@nexr.com>
2012-04-30 10:50:30 +0900

    Modify the package info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(name='HiveFDW',
       version='0.0.1',
       description='Hive FDW for PostgreSQL',
-      author='Brandon.Kim',
-      author_email='brandon.kim@nexr.com',
+      author='Youngwoo Kim',
+      author_email='warwithin@gmail.com',
       url='https://github.com/youngwookim/hive-fdw-for-postgresql',
       py_modules=['hivefdw'])
